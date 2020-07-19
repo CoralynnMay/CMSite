bc9f0f6886246bed8d9aaac1fd22149ff7f30a81
Coralynn Colombe <su@coralynn.net>
2020-07-19 10:56:39 -0400

    round out hero and nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'CoralynnMay';
+
+  @HostBinding('class.trianglevisible') isTriangleVisible: boolean;
+  @HostListener('window:scroll', [])
+  public setTriangleVisible(): void {
+    this.isTriangleVisible = 10 > document.documentElement.scrollTop;
+  }
+
+  ngOnInit(): void {}
 }
